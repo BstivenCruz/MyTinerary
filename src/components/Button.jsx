123de4268bf5763a23b7dd7b90ca9d634f430d2a
stiven_cruz <bseguracruz@yahoo.com>
2023-05-09 16:29:40 -0500

    fix and responsive

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,10 @@
+/* eslint-disable react/prop-types */
 // eslint-disable-next-line react/prop-types
-export default function Button({text}) {
+import { Link as Anchor } from "react-router-dom";
+export default function Button({text,to}) {
   return (
-    <div className="flex justify-center rounded-[10px] py-3 bg-botonR text-white w-[80%]">
-      <p>{text}</p>
+    <div className="flex justify-center cursor-pointer rounded-[10px] pt-4 pb-2 bg-botonR text-white w-[80%]">
+      <Anchor to={to}>{text}</Anchor>
     </div>
   );
 }
